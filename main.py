2e96e651c1393a9dbc7367252c4cc6ed7348dc97
Jasmine Arellano <jasmineguerra178@gmail.com>
2025-11-21 20:33:26 +0000

    Fixing syntax error

--- main.py
+++ main.py
@@ -71,11 +71,7 @@ async def post_inference(data: Data):
         categorical_features=cat_features,
         label,
         encoder=encoder,
-        training=False
-        # your code here
-        # use data as data input
-        # use training = False
-        # do not need to pass lb as input
+        training=False,
     )
     _inference = inference(model, data_processed) # your code here to predict the result using data_processed
     return {"result": apply_label(_inference)}
